5fe5c6313460e771c7d36531cff172677260f8ed
wesley-a-leung <wesley.a.leung@gmail.com>
2017-06-15 23:07:08 -0400

    Replaced tabs with spaces

--- Election Predictor/src/ElectionNight.java
+++ Election Predictor/src/ElectionNight.java
@@ -2,12 +2,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ElectionNight {
-	
-	private static Date date = new Date();
-	private static SimpleDateFormat ft = new SimpleDateFormat ("D");
+    
+    private static Date date = new Date();
+    private static SimpleDateFormat ft = new SimpleDateFormat ("D");
 
-	public static void main(String[] args) {
-		String arguments[] = {ft.format(date), "data/statesElectionNight.dat", "20000"};
-		Election.main(arguments);
-	} // main method
+    public static void main(String[] args) {
+        String arguments[] = {ft.format(date), "data/statesElectionNight.dat", "20000"};
+        Election.main(arguments);
+    } // main method
 } // ElectionNight class
